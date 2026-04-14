c76dc9044a0d792fb8da9b18bc3702be08d1edeb
Jumeaux <jumeaux.david.formation@gmail.com>
2026-04-14 15:36:26 +0200

    lien excel cors

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ require("./db");
 
 const app = express();
 
-app.use(cors({
-origin:"http://localhost:5173"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
