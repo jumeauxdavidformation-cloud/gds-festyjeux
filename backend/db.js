94205c60b244a572acb73ae03ff764efd0f499db
Jumeaux <jumeaux.david.formation@gmail.com>
2026-04-14 22:04:44 +0200

    fix db

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,31 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
 
 host: process.env.MYSQLHOST,
 user: process.env.MYSQLUSER,
 password: process.env.MYSQLPASSWORD,
 database: process.env.MYSQLDATABASE,
-port: process.env.MYSQLPORT
+port: process.env.MYSQLPORT,
+
+waitForConnections: true,
+connectionLimit: 10,
+queueLimit: 0
 
 });
 
-db.connect((err)=>{
+db.getConnection((err, connection)=>{
 
 if(err){
 
-console.log("erreur connexion mysql");
+console.log("ERREUR MYSQL");
 console.log(err);
 
 }else{
 
-console.log("connecté à mysql railway");
+console.log("MYSQL CONNECTE");
+
+connection.release();
 
 }
 
